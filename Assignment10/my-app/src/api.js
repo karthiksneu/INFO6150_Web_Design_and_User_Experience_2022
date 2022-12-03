21bf8c606a46fe5d659e35284eee4a8ca243f413
karthiksneu <sunil.k@northeastern.edu>
2022-12-02 21:06:46 -0500

    commit

---
+++ Assignment10/my-app/src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd8f11927ddmsh64b8eb2677eb375p1816f1jsn4680d2a02dc8',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "34bfbe6dd9a694547bc6a2bdd93b2eee";
+
